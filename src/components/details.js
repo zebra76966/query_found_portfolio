4c20c8df68ed137412624fa8d39bf8cd54fbc9ec
zebra76966 <rs288442@gmail.com>
2024-08-02 01:15:13 +0530

    added details section

--- src/components/details.js
+++ src/components/details.js
@@ -20,8 +20,11 @@ const Details = ({ data, setCurentProj }) => {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.1 }}
       >
-        <div className="p-3 bgcolor-plain d-flex align-items-center justify-content-between position-sticky top-0 start-0" style={{ zIndex: "99", width: "90%", borderRadius: "0 0 20px 20px" }}>
-          <div className="d-flex align-items-center w-50">
+        <div
+          className="p-3 ps-lg-3 ps-5 bgcolor-plain d-md-flex align-items-center justify-content-between text-lg-auto text-end position-sticky top-0 start-0"
+          style={{ zIndex: "99", width: "90%", borderRadius: "0 0 20px 20px" }}
+        >
+          <div className="d-flex align-items-center justify-content-md-start justify-content-end topBar ps-md-0 ps-4">
             <button
               onClick={() => {
                 setCurentProj(0);
@@ -33,7 +36,7 @@ const Details = ({ data, setCurentProj }) => {
             <p className="py-0 my-0 ms-2 fs-5 txtcolor-secondary">Go back to Projects</p>
           </div>
 
-          <button target="_blank" className="btn fs-5 p-3 px-4 bgcolor-secondary txtcolor-primary rounded-pill border border-1 secondary-border ms-auto get-in-touch">
+          <button target="_blank" className="btn fs-5 p-lg-3 p-2 px-lg-4 px-3 bgcolor-secondary txtcolor-primary rounded-pill border border-1 secondary-border ms-auto get-in-touch">
             <div className="pulse"></div>
             <div className="pulse"></div>
             <div className="pulse"></div>
@@ -43,18 +46,12 @@ const Details = ({ data, setCurentProj }) => {
           </button>
         </div>
       </motion.div>
-      <motion.div
-        className="ms-auto pe-3 pt-2  ps-0 position-relative"
-        style={{ width: "90%" }}
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-      >
+      <motion.div className="ms-auto pe-3 pt-2  ps-0 position-relative portDiv" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.1 }}>
         <div className="cotainer-fluid  d-flex align-items-center mb-4 pb-5" style={{ height: "75dvh" }}>
           <div className="row ">
             <motion.div className="col-lg-8" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }}>
               <div className="w-100 txtcolor-secondary">
-                <h1 className="display-1 py-3 fw-bold">{data.name}</h1>
+                <h1 className="display-1 pt-lg-3 pb-3 pt-5 mt-lg-0 mt-5  fw-bold">{data.name}</h1>
                 <p className="display-5 mt-4">{data.small_desc}</p>
               </div>
             </motion.div>
@@ -63,14 +60,14 @@ const Details = ({ data, setCurentProj }) => {
               <img src={data.images[0]} className="roundedimg shadow" />
             </motion.div>
 
-            <div className="col-lg-6 text-start mt-5 pt-5 ">
+            <div className="col-lg-6 text-start mt-lg-5 pt-lg-5 pt-3 mt-3">
               <div className="w-100 d-flex justify-content-between">
                 <a href={data.url} target="_blank" className="btn btn-lg fs-3 p-3 px-5 bgcolor-secondary txtcolor-primary me-2 rounded-pill border border-1 secondary-border">
                   <span className="me-2"> Visit Website </span>
                   <i className="fa fa-external-link-square fs-2"></i>
                 </a>
 
-                <div class="scrolldown">
+                <div class="scrolldown d-lg-block d-none">
                   <div class="chevrons">
                     <div class="chevrondown"></div>
                     <div class="chevrondown"></div>
@@ -78,8 +75,8 @@ const Details = ({ data, setCurentProj }) => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-6 text-end mt-5 pt-5">
-              <div className="w-75 ms-auto">
+            <div className="col-lg-6 text-lg-end mt-lg-5 pt-lg-5 pt-3 mt-3">
+              <div className="w-75 ms-lg-auto">
                 {data.tech_stack.map((ini) => {
                   return <button className="btn btn-lg bgcolor-plain txtcolor-secondary me-2 rounded-pill border border-1 secondary-border mb-1">{ini}</button>;
                 })}
@@ -90,21 +87,21 @@ const Details = ({ data, setCurentProj }) => {
 
         <div className="cotainer-fluid ">
           <div className="row ">
-            <div className="col-12 my-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+            <div className="col-12 mt-lg-4 mb-lg-4 mt-5 mb-0" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
               <img src={data.images[0]} className="roundedimg main shadow" />
             </div>
 
-            <motion.div className="col-12 my-5 pt-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+            <motion.div className="col-12 my-lg-5 my-2 pt-lg-4 pt-3" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
               <div className="w-100 txtcolor-secondary">
                 <h1 className="display-4 py-2 fw-bold">Description</h1>
                 <p className="fs-2 mt-1"> {data.desc} </p>
               </div>
             </motion.div>
 
-            <motion.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} className="col-md-6 my-5 ">
+            <motion.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} className="col-md-6 my-lg-5 my-3 ">
               <img src={data.images[1]} className="roundedimg bodyimg shadow" />
             </motion.div>
-            <motion.div className="col-md-6 my-5" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+            <motion.div className="col-md-6 my-lg-5 my-3" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
               <img src={data.images[2]} className="roundedimg bodyimg shadow" />
             </motion.div>
 
@@ -112,14 +109,14 @@ const Details = ({ data, setCurentProj }) => {
               <div className="w-100 txtcolor-secondary my-4">
                 <h1 className="display-4 py-2 fw-bold">Key Features</h1>
               </div>
-              <div className={`features-section ${stopanim ? "" : "animated"} my-5`}>
+              <div className={`features-section ${stopanim ? "" : "animated"} my-lg-5 my-3`}>
                 <div className="features-section-inner" onMouseOver={() => setStopAnim(true)} onMouseLeave={() => setStopAnim(false)}>
                   {data.features.map((ini, i) => {
                     return (
                       <div className={`featurecard ${i % 2 == 0 ? "txtcolor-secondary bgcolor-plain" : "txtcolor-plain bgcolor-secondary"} shadow`}>
                         <h1 className="display-6 py-2 fw-bold">{ini.name}</h1>
 
-                        <p className="thinScroll lead mt-4 fw-bold">{ini.detail} </p>
+                        <p className="thinScroll lead mt-md-4 mt-2 fw-bold">{ini.detail} </p>
 
                         <div className={`bottom-star ${i % 2 == 0 ? "txtcolor-secondary " : "txtcolor-plain"}`}>
                           <i className="fa fa-star fs-1"></i>
@@ -132,7 +129,7 @@ const Details = ({ data, setCurentProj }) => {
                       <div className={`featurecard ${i % 2 == 0 ? "txtcolor-secondary bgcolor-plain" : "txtcolor-plain bgcolor-secondary"} shadow`}>
                         <h1 className="display-6 py-2 fw-bold">{ini.name}</h1>
 
-                        <p className="thinScroll lead mt-4 fw-bold">{ini.detail} </p>
+                        <p className="thinScroll lead  mt-md-4 mt-2 fw-bold">{ini.detail} </p>
                         <div className={`bottom-star ${i % 2 == 0 ? "txtcolor-secondary " : "txtcolor-plain"}`}>
                           <i className="fa fa-star fs-1"></i>
                         </div>
@@ -143,29 +140,29 @@ const Details = ({ data, setCurentProj }) => {
               </div>
             </motion.div>
 
-            <motion.div className="col-12 mt-5" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+            <motion.div className="col-12 mt-md-5 mt-3" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
               <img src={data.images[3]} className="roundedimg main shadow" />
             </motion.div>
 
-            <motion.div className="col-md-6 my-5 pe-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+            <motion.div className="col-md-6 my-md-5 my-3 pe-md-4 " initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
               <img src={data.images[1]} className="roundedimg bodyimg shadow" />
             </motion.div>
-            <motion.div className="col-md-6 my-5 ps-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+            <motion.div className="col-md-6 my-md-5 my-3 ps-md-4 " initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
               <img src={data.images[2]} className="roundedimg bodyimg shadow" />
             </motion.div>
 
             {/* Final Thoughts */}
             <motion.div className="col-12 mt-5" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
-              <div className="w-100 txtcolor-secondary my-5 py-5">
+              <div className="w-100 txtcolor-secondary my-md-5 my-2 py-md-5 py-2">
                 <div className="d-flex align-items-center justify-content-center">
-                  <div class="eye-container me-4">
+                  <div class="eye-container me-lg-4 me-2">
                     <div class="eye">
                       <div class="eyelid-wrapper">
                         <div class="eyelid blink1"></div>
                       </div>
                     </div>
                   </div>
-                  <div class="eye-container ms-4">
+                  <div class="eye-container ms-lg-4 ms-2">
                     <div class="eye">
                       <div class="eyelid-wrapper">
                         <div class="eyelid blink1"></div>
@@ -173,7 +170,7 @@ const Details = ({ data, setCurentProj }) => {
                     </div>
                   </div>
                 </div>
-                <h1 className="display-5 py-4 fw-bold text-center txtcolor-secondary">
+                <h1 className="display-lg-5 display-6 py-4 fw-bold text-center txtcolor-secondary">
                   Looking for something <u>SPECIFIC</u> or <u>LIKE THIS</u>?
                 </h1>
 
