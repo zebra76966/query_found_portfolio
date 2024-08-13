7c0462ef808c7b54ba87fa3d6b32efd52b2381ab
zebra76966 <rs288442@gmail.com>
2024-08-13 15:45:28 +0530

    added converter efficiency

--- src/components/all_services.js
+++ src/components/all_services.js
@@ -17,16 +17,28 @@ const AllServices = ({ setCurentProj }) => {
         <h1 className="display-1  pt-xxl-3 pb-md-3 pt-2 pb-3 fw-bold txtcolor-secondary">Our Services</h1>
 
         <p className="lead txtcolor-secondary">
-          We offer a comprehensive range of services to meet all your digital needs. Our team of experts is dedicated to delivering top-notch solutions that drive growth and innovation.
+          At Bharat Web Dev, we deliver comprehensive digital solutions that drive innovation and growth. Our expertise spans full-stack development, backend and frontend development, UI/UX design,
+          AI-driven solutions, data science, and machine learning algorithms. We also offer graphics design, video editing, custom CRM automation, legacy code debugging, and website redesign and
+          rebuilding. Our goal is to provide seamless, scalable, and tailored solutions that enhance your digital presence and operational efficiency.
         </p>
       </div>
       <div className="w-100 mt-xxl-4 mt-md-5 tags pt-5 pb-3 ps-xxl-0 ps-2 text-xxl-start text-start ">
         <button className="btn bgcolor-secondary txtcolor-primary me-2 rounded-pill mb-2 border border-1 secondary-border">All</button>
-        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">E-commerce</button>
-        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Web Design</button>
-        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">UI/UX</button>
-        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Graphic Design</button>
-        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">AI/Prompt Engineering</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Django</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Flask</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">React</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Node.js</button>
+
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">AWS</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Azure</button>
+
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Nginx</button>
+
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Apache</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">WordPress</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">WooCommerce </button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">Webflow</button>
+        <button className="btn bgcolor-plain txtcolor-secondary me-2 rounded-pill mb-2 border border-1 secondary-border ">PHP CodeIgniter</button>
       </div>
 
       <div className="cotainer-fluid pb-5 p-md-0  p-3">
